3a9b9ae6efbe27594cd8bbf52246ed82e15fe487
nedialkom <nedialkom@gmail.com>
2019-05-01 23:02:03 +0300

    6. Documentation

--- polls/urls.py
+++ polls/urls.py
@@ -5,6 +5,14 @@ from .apiviews import PollViewSet
 
 from rest_framework.authtoken import views
 
+from rest_framework.documentation import include_docs_urls
+
+from django.contrib.auth.views import LoginView
+
+from rest_framework_swagger.views import get_swagger_view
+schema_view = get_swagger_view(title='Polls API')
+
+
 router = DefaultRouter()
 router.register('polls', PollViewSet, base_name='polls')
 
@@ -14,6 +22,9 @@ urlpatterns = [
     path("polls/<int:pk>/choices/", ChoiceList.as_view(), name="choice_list"),
     path("polls/<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(), name="create_vote"),
     path("users/", UserCreate.as_view(), name="user_create"),
+    path('swagger-docs/', schema_view),
+    path('accounts/login/', LoginView.as_view(template_name='login.html'), name="login"),
+    path('docs/', include_docs_urls(title='Polls API')),
 ]
 
 urlpatterns += router.urls
--- pollsapi/settings.py
+++ pollsapi/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'polls',
     'rest_framework.authtoken',
+    'rest_framework_swagger',
 ]
 
 MIDDLEWARE = [
